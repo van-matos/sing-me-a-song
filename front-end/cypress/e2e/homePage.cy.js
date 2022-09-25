88b5883bab9793abaa738fadffce62d3221f7508
van-matos <vanmatos@poli.ufrj.br>
2022-09-25 20:34:51 -0300

    test: post repeated recommendation e2e test

--- front-end/cypress/e2e/homePage.cy.js
+++ front-end/cypress/e2e/homePage.cy.js
@@ -19,8 +19,12 @@ describe("Home page", () => {
     cy.intercept("GET", "http://localhost:5000/recommendations").as(
       "getRecommendations"
     );
-    cy.wait("@getRecommendations");
-    cy.get("article").should("have.length.of.at.most", 10);
+    cy.wait("@getRecommendations").then((interception) => {
+      const status = interception.response.statusCode;
+      const length = interception.response.body.length;
+      expect(length).to.be.below(11);
+      expect(status).eq(200);
+    });
   });
 
   it("Post recommendation", () => {
@@ -28,11 +32,31 @@ describe("Home page", () => {
     const youtubeLink = `https://youtu.be/${faker.random.alpha(11)}`;
 
     cy.visit("http://localhost:3000");
-    cy.get('input[placeholder="Name"]').type(name);
-    cy.get('input[placeholder="https://youtu.be/..."]').type(youtubeLink);
-    cy.intercept("POST", "/recommendations").as("createRecommendation");
+    cy.get('[data-test-id="test-input-name"]').type(name);
+    cy.get('[data-test-id="test-input-youtubeLink"]').type(youtubeLink);
+    cy.intercept("POST", "/recommendations").as("postRecommendation");
     cy.get("button").click();
-    cy.wait("@createRecommendation");
-    cy.contains(name).should("be.visible");
+    cy.wait("@postRecommendation").then((interception) => {
+      const status = interception.response.statusCode;
+      expect(status).eq(201);
+    });
+  });
+
+  it("Post repeated recommendation", () => {
+    const name = faker.music.songName();
+    const youtubeLink = `https://youtu.be/${faker.random.alpha(11)}`;
+
+    cy.visit("http://localhost:3000");
+    cy.get('[data-test-id="test-input-name"]').type(name);
+    cy.get('[data-test-id="test-input-youtubeLink"]').type(youtubeLink);
+    cy.get("button").click();
+    cy.intercept("POST", "/recommendations").as("postRecommendation");
+    cy.get('[data-test-id="test-input-name"]').type(name);
+    cy.get('[data-test-id="test-input-youtubeLink"]').type(youtubeLink);
+    cy.get("button").click();
+    cy.wait("@postRecommendation").then((interception) => {
+      const status = interception.response.statusCode;
+      expect(status).eq(409);
+    });
   });
 });
